41849a69e539d48d064ba52fa08525a5d03d7c23
nguyenson <sonpfievk57@gmail.com>
2019-08-14 18:30:32 +0700

    format header file

--- ios/mobile-Bridging-Header.h
+++ ios/mobile-Bridging-Header.h
@@ -1,13 +1,1 @@
-//
-//  mobile-Bridging-Header.h
-//  mobile
-//
-//  Created by nguyenson on 8/14/19.
-//  Copyright © 2019 Facebook. All rights reserved.
-//
 
-#ifndef mobile_Bridging_Header_h
-#define mobile_Bridging_Header_h
-
-
-#endif /* mobile_Bridging_Header_h */
